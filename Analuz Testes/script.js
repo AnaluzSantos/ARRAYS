8eef6b4fe6524119d70dec95fcb29a940eb0166d
Analuz Santos <108814723+AnaluzSantos@users.noreply.github.com>
2023-04-10 12:19:16 -0300

    Add files via upload

---
+++ Analuz Testes/script.js
@@ -0,0 +1,32 @@
+// const variableName = []; 
+// const users = ["Ana", "Fernando", "Rafael", "Clara", "Maria"];
+// users.push("Roger", "Carol")
+// console.log(users); 
+
+// const person = ["Felipe", 30, "São Paulo", "Masculino", "22233366689"];
+// person.pop(); 
+// console.log(person); 
+
+// const fruits = ["Apple", "Orange", "Banana", "Pear"];
+// fruits.shift();
+// console.log(fruits);
+
+// const jobs = ["Frontend", "Backend", "DevOps", "FullStack"]; 
+// jobs.unshift("Design"); 
+// console.log(jobs);
+
+// const jobs = ["Frontend", "Backend", "DevOps", "FullStack"]; 
+// let indexOf = jobs.indexOf("FullStack");
+// console.log(indexOf); 
+
+// const fruits = ["Apple", "Orange", "Banana", "Pear"]; 
+// let indexOfSearch = fruits.indexOf("Banana", 3);
+// console.log(indexOfSearch);
+
+// const jobs = ["Frontend", "Backend", "DevOps", "FullStack"];
+// jobs.sort();
+// console.log(jobs); 
+
+const colors = ["Red", "Green", "White", "Black"]; 
+let join = colors.join("* "); 
+console.log(join); 
---
+++ Analuz Testes/script2.js
@@ -0,0 +1,27 @@
+// const variableName = [];
+// const users = ["Arthur", "Fernanda", "Cristian", "Isabella", "Emilly", "Glaucos", "Edrielle", "Analuz"];
+// let join = users.join("* ");
+// console.log(join);
+
+// const users = ["Arthur", "Fernanda", "Cristian", "Isabella", "Emilly", "Glaucos", "Edrielle", "Analuz"]; 
+// users.sort();
+// console.log(users); 
+
+// function myFunction(a, b){
+//     let array = [];
+
+//     if(a.indexOf(b) !== -1){
+//         array.push(b + a);
+//         return array;
+//     } else {
+//         array.push(a + b);
+//         return array;
+//     }
+// }
+
+// console.log(myFunction("types", "a"))
+
+const groupPop = ["Zayn", "Harry", "Louis", "Liam", "Niall"];
+groupPop.shift();
+console.log(groupPop); 
+
