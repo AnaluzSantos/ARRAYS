cebf73477bccf5b3b972de0c2ed2118927a6c55b
Analuz Santos <108814723+AnaluzSantos@users.noreply.github.com>
2023-04-15 11:25:35 -0300

    Add files via upload

---
+++ Array - Atividades/script.js
@@ -0,0 +1,13 @@
+// const person = ["Perrie", "Jade", "Leigh-Anne", "Jesy"];
+// person.pop();
+
+// console.log(person);
+
+// const person = ["Bella", "Edward"];
+// person.unshift("Jacob");
+
+// console.log(person)
+
+const person = ["Simon", "Alvin", "Theodore"];
+person.push("David");
+console.log(person)
